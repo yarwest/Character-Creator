7624bc6563f141c341b6832a9183332a3bbeaa71
yarwest <yarno1998@gmail.com>
2016-07-29 13:01:53 +0200

    MORE STUFFFFFFF!!!!!
    Sprites: added sprite with another, darker, skin color.
    CSS: made all left buttons not display on load
    HTML: added skin color option, added classes to buttons, changed
    parameters for onclick change function, added random button.
    JS: (oh boy) added some arrays, added assignSource function which just
    reassings all image sources, removed split from change function and
    added parameter, call assignSource if object is skin, otherwise just
    replace one image. Made a huge function which checks if the current
    value is at the edge of the array and changes the display of buttons
    based on that. Added a random function which just calls getRandomInt to
    get ints between the borders of the array, then runs both assignSource
    and boundaryCheck. getRandomInt returns a random number between the min
    and max parameters.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 var headColor = ['blonde', 'brown', 'black'];
 var chestColor = ['white', 'black', 'yellow'];
 var legsColor = ['blue', 'grey', 'green'];
-var skinColor = ['1'];
+var skinColor = ['1', '2'];
+
+var headType = ['1'];
+var chestType = ['1'];
+var legsType = ['1'];
 
 var currentHeadColor = 0;
 var currentChestColor = 0;
@@ -12,25 +16,164 @@ var currentHeadType = 0;
 var currentChestType = 0;
 var currentLegsType = 0;
 
-var headType = ['1'];
-var chestType = ['1'];
-var legsType = ['1'];
+function assignSource () {
+    document.getElementById('head').src='sprites/head/' + headType[currentHeadType] + '-' + headColor[currentHeadColor] + '-' + skinColor[currentSkinColor] + '.png'; 
+    document.getElementById('chest').src='sprites/chest/' + chestType[currentChestType] + '-' + chestColor[currentChestColor] + '-' + skinColor[currentSkinColor] + '.png'; 
+    document.getElementById('legs').src='sprites/legs/' + legsType[currentLegsType] + '-' + legsColor[currentLegsColor] + '-' + skinColor[currentSkinColor] + '.png';
+}
 
-function change(object, direction) {
-    var split = object.split("_");
-    var lowcaseSplit = split[0].toLowerCase();
-    var variable = "current" + split[0] + split[1];
+function change(object, change, direction) {
+    var lowcaseSplit = object.toLowerCase();
+    var variable = "current" + object + change;
     if(direction === 'left') {
         eval(variable + "--");
     } else if(direction === 'right') {
         eval(variable + "++");
     }
-    var type = eval(lowcaseSplit + "Type[current" + split[0] + "Type]");
-    var color = eval(lowcaseSplit + "Color[current" + split[0] + "Color]");
-    var skin = skinColor[currentSkinColor];
-    document.getElementById(lowcaseSplit).src='sprites/' + lowcaseSplit + '/' + type + '-' + color + '-' + skin + '.png';
+    if(lowcaseSplit === 'skin') {
+        assignSource();
+    }
+    else {
+        var type = eval(lowcaseSplit + "Type[current" + object + "Type]");
+        var color = eval(lowcaseSplit + "Color[current" + object + "Color]");
+        var skin = skinColor[currentSkinColor];
+        document.getElementById(lowcaseSplit).src='sprites/' + lowcaseSplit + '/' + type + '-' + color + '-' + skin + '.png';
+    }
+    boundaryCheck(object, change, direction);
+}
+
+function boundaryCheck(object, change, direction) {
+    if(object === 'Head') {
+        if(change === 'Color') {
+            if(direction === 'left') {
+                if(currentHeadColor === 0) {
+                    document.getElementById('head-color-left').style.display="none";
+                }
+                document.getElementById('head-color-right').style.display="inline-block";
+            }
+            else {
+                if (currentHeadColor === headColor.length-1) {
+                    document.getElementById('head-color-right').style.display="none";
+                }
+                document.getElementById('head-color-left').style.display="inline-block";
+            }
+        }
+    }
+    if(object === 'Chest') {
+        if(change === 'Color') {
+            if(direction === 'left') {
+                if(currentChestColor === 0) {
+                    document.getElementById('chest-color-left').style.display="none";
+                }
+                document.getElementById('chest-color-right').style.display="inline-block";
+            }
+            else {
+                if (currentChestColor === chestColor.length-1) {
+                    document.getElementById('chest-color-right').style.display="none";
+                }
+                document.getElementById('chest-color-left').style.display="inline-block";
+            }
+        }
+    }
+    if(object === 'Legs') {
+        if(change === 'Color') {
+            if(direction === 'left') {
+                if(currentLegsColor === 0) {
+                    document.getElementById('legs-color-left').style.display="none";
+                }
+                document.getElementById('legs-color-right').style.display="inline-block";
+            }
+            else {
+                if (currentLegsColor === legsColor.length-1) {
+                    document.getElementById('legs-color-right').style.display="none";
+                }
+                document.getElementById('legs-color-left').style.display="inline-block";
+            }
+        }
+    }
+    if(object === 'Skin') {
+        if(direction === 'left') {
+            if(currentSkinColor === 0) {
+                document.getElementById('skin-color-left').style.display="none";
+            }
+            document.getElementById('skin-color-right').style.display="inline-block";
+        }
+        else {
+            if (currentSkinColor === skinColor.length-1) {
+                document.getElementById('skin-color-right').style.display="none";
+            }
+            document.getElementById('skin-color-left').style.display="inline-block";
+        }
+    }
+    if(object === 'All') {
+        if(currentHeadColor === 0) {
+            document.getElementById('head-color-left').style.display="none";
+        }
+        else {
+            document.getElementById('head-color-left').style.display="inline-block";
+        }
+        if (currentHeadColor === headColor.length-1) {
+            document.getElementById('head-color-right').style.display="none";
+        }
+        else {
+            document.getElementById('head-color-right').style.display="inline-block";
+        }
+        
+        if(currentChestColor === 0) {
+            document.getElementById('chest-color-left').style.display="none";
+        }
+        else {
+            document.getElementById('chest-color-left').style.display="inline-block";
+        }
+        if (currentChestColor === chestColor.length-1) {
+            document.getElementById('chest-color-right').style.display="none";
+        }
+        else {
+            document.getElementById('chest-color-right').style.display="inline-block";
+        }
+        
+        if(currentLegsColor === 0) {
+            document.getElementById('legs-color-left').style.display="none";
+        }
+        else {
+            document.getElementById('legs-color-left').style.display="inline-block";
+        }
+        if (currentLegsColor === legsColor.length-1) {
+            document.getElementById('legs-color-right').style.display="none";
+        }
+        else {
+            document.getElementById('legs-color-right').style.display="inline-block";
+        }
+        
+        if(currentSkinColor === 0) {
+            document.getElementById('skin-color-left').style.display="none";
+        }
+        else {
+            document.getElementById('skin-color-left').style.display="inline-block";
+        }
+        if (currentSkinColor === skinColor.length-1) {
+            document.getElementById('skin-color-right').style.display="none";
+        }
+        else {
+            document.getElementById('skin-color-right').style.display="inline-block";
+        }
+    }
 }
 
 function random() {
+    currentHeadType = getRandomInt(0, headType.length-1);
+    currentChestType = getRandomInt(0, chestType.length-1);
+    currentLegsType = getRandomInt(0, legsType.length-1);
     
+    currentHeadColor = getRandomInt(0, headColor.length-1);
+    currentChestColor = getRandomInt(0, chestColor.length-1);
+    currentLegsColor = getRandomInt(0, legsColor.length-1);
+    currentSkinColor = getRandomInt(0, skinColor.length-1);
+    
+    assignSource();
+    boundaryCheck('All', null, null);
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
